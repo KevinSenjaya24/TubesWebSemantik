da1c16ad751efb3c4435e399615d2f808b28d4fa
KevinSenjaya24 <50093451+KevinSenjaya24@users.noreply.github.com>
2021-06-19 18:28:57 +0700

    Changes web.php and Delete File
    Saya memakai project tugas praktikum yang lalu memakai database, jadi saya coba commit lagi delete family dan jemaat controller agar tidak error nanti

--- routes/web.php
+++ routes/web.php
@@ -32,20 +32,4 @@ Route::post('/sparqld', 'Admin\RssController@describe')->name('admin.describe.up
 
 Route::post('/sparqladc', 'Admin\RssController@ask')->name('admin.ask.update');
 
-Route::get('/family', 'Admin\FamilyController@index')->name('admin.family.family');
-Route::get('/family/{id}', 'Admin\FamilyController@details')->name('admin.family.detail');
-Route::get('/family/show/{id}', 'Admin\FamilyController@show')->name('admin.family.show');
-Route::post('/family', 'Admin\FamilyController@update')->name('admin.family.update');
-Route::delete('/family/delete/{id}','Admin\FamilyController@delete')->name('admin.family.delete');
 
-
-Route::get('/jemaat', 'Admin\JemaatController@index')->name('admin.jemaat.jemaat');
-Route::get('/jemaat/{id}', 'Admin\JemaatController@details')->name('admin.jemaat.detail');
-Route::post('/jemaat', 'Admin\JemaatController@update')->name('admin.jemaat.update');
-Route::delete('/jemaat/delete/{id}','Admin\JemaatController@delete')->name('admin.jemaat.delete');
-
-	
-Route::get('/family.xml', 'SitemapController@indexFamily');
-
-
-Route::get('/jemaat.xml', 'SitemapController@indexJemaat');
